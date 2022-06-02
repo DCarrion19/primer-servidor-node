7e44ea769283990696e3160ee9b66d36e2e839fd
DCarrion <dcarrionn@gmail.com>
2022-06-01 20:00:34 -0500

    Ya estamos ready para subir a produccion

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require('express')
-
 const app = express()
+const fs = require('fs')
+const multer  = require('multer')
+const sharp = require('sharp');
+
+const storageStrategy = multer.memoryStorage()
+const upload = multer({ Storage: storageStrategy })
+
+app.use(express.json())
 
 app.get('/', function(req, res) {
 
-    res.send('Hola Mundo')
+    res.send('Hola Mundo !!!!!!!')
 }
 
 )
 
-app.listen(3000)
+app.post('/imagen', upload.single('imagen'), async function (req, res) {
+
+    
+    const imagen = req.file
+
+    const processedImage = sharp(imagen.buffer)
+
+    const resizeImage = processedImage.resize(800, 200, {
+        fit: "contain",
+        background: "#fff"
+    })
+    
+    const resizeImageBuffer = await resizeImage.toBuffer()
+
+    fs.writeFileSync('nuevaruta/prueba.png', resizeImageBuffer)
+
+    console.log(resizeImageBuffer)
 
 
+res.send({ resizeImage: resizeImageBuffer})
+
+})
+
+const port = process.env.port || 4000
+
+app.listen(port, function(){
+
+console.log("Servidor escuchando en el puerto", port)
+
+})
